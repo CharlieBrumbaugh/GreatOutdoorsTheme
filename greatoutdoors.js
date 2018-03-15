efe0a00a5087e61448147c85566ac254ce350e84
Charlie Brumbaugh <12149031+CharlieBrumbaugh@users.noreply.github.com>
2018-03-14 20:51:22 -0600

    Create greatoutdoors.js

---
+++ greatoutdoors.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name          Great Outdoors Theme
+// @namespace     The Great Outdoors
+// @description   Simple theme enhancements for the Outdoors.SE
+// @include       https://outdoors.stackexchange.com*
+// @grant          GM_addStyle
+// ==/UserScript==
+
+GM_addStyle(`
+           .post-tag,.post-tag:hover{
+               border: 1px solid brown;
+}
+.question-hyperlink:visited, .answer-hyperlink:visited, #hot-network-questions ul a:visited{
+    color: brown;
+}
+.vote-up-on {
+    background-position: 0px;
+    background-image: url(https://i.stack.imgur.com/oTdiU.png);
+    background-size: 40px;
+}
+           `);
+console.log('ran');
